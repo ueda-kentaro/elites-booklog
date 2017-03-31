23035ed74e541d8401cb1c63250ea2b616964b27
ueda kentaro <nowallcloud9+ueda@gmail.com>
2017-03-31 15:03:35 +0000

    BookのCRUD機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'books#index'
+  resources :books
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
---
+++ spec/factories/books.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :book do
+    title "MyString"
+    user_id 1
+    author "MyString"
+    publisher "MyString"
+    price 1
+    publish_date "2017-03-31 10:52:32"
+    caption "MyText"
+    image "MyString"
+  end
+end
